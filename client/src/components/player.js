1f4258c330ab615865a427dacfd056feb9afb7d5
Bucora <lubos.martincek2@gmail.com>
2021-12-03 18:04:57 +0100

    pridane features playera, pokus o Ovladaci panel

--- client/src/components/player.js
+++ client/src/components/player.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Player.module.scss';
+import Slider from '@mui/material/Slider';
+import FastForwardIcon from '@mui/icons-material/FastForward';
+import FastRewindIcon from '@mui/icons-material/FastRewind';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import StopCircleIcon from '@mui/icons-material/StopCircle';
 export const Player = () => {
+    const [slider, setSlider] = useState(0);
+    const [playing, setPlaying] = useState(false);
     return (
         <div className={styles.wrapper}>
-            <div class={styles.controls}>
-                <button > 
-                    <span> </span>
-                    <span></span>
-                </button>
-            </div>
+            <div class={styles.controls} >
+                <Slider defaultValue={0} setp={1} min={0} max={100} onChange={e => {setSlider(e.target.value)}} value={slider} />
+            </div><br></br>
+            <div class={styles.buttons}>
+                <FastRewindIcon />
+                {!playing && <PlayCircleIcon onClick={e => {setPlaying(true)}}/>}
+                {playing && <StopCircleIcon onClick={e => {setPlaying(false)}}/>}
+                <FastForwardIcon />
+                <p>{slider}%</p>
+            </div>    
+            
         </div>
     )
 }
