79c4581c823e4704059475aa460a4294fe9ca393
Bucora <lubos.martincek2@gmail.com>
2021-12-03 21:28:33 +0100

    Added some sidepanel functionality

--- client/src/components/SidePanel.js
+++ client/src/components/SidePanel.js
@@ -2,10 +2,26 @@
 import styles from './SidePanel.module.scss';
 
 export const SidePanel = (props) => {
+  
+
+  //Placeholder list, nahradi ho list s playlistami
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number, index) => (
+    <button className={props.currPlaylist===number ? styles.activeButton : ""} onClick={e => {props.pageHandler("Playlist"); props.playlistHandler(number)}}><li key={index}> {number} </li></button>
+  ));
+
 
   return (
     <div className={`${styles.SidePanel} ${props.className}`}>
-      <h1>Hej dopice</h1>
+      <h1>Edvánsd Mjúzik Pleja</h1>
+      <div >
+        <button className={props.page==="Profile" ? styles.activeButton : ""} onClick={e => {props.pageHandler("Profile"); props.playlistHandler("")}}>My profile</button>
+        <button className={props.page==="NewPlaylist" ? styles.activeButton : ""} onClick={e => {props.pageHandler("NewPlaylist"); props.playlistHandler("")}}>New playlist</button>
+      </div>
+      <div>
+        <h2>My playlists</h2>
+        <ul>{listItems}</ul>
+      </div>
 
     </div >
   )
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,19 +1,37 @@
 import styles from './Home.module.scss';
-import { Link } from 'react-router-dom';
 import Profile from '../Profile/Profile';
 import { SidePanel } from '../../components/SidePanel';
 import { Player } from '../../components/Player';
+import { useState } from 'react';
 
 const Home = (props) => {
+  const [page, setPage] = useState("Home");
+  const [currPlaylist, setCurrPlaylist] = useState([]);
+  const pageHandler = (foo) => {
+    setPage(foo)
+  }
+  const playlistHandler = (foo) => {
+    setCurrPlaylist(foo)
+  }
   return (
     <div className={styles.Home}>
       <div className={styles.flexRow}>
-        <SidePanel className={styles.SidePanel} />
-        <Profile
+        <SidePanel className={styles.SidePanel} page={page} pageHandler={pageHandler} playlistHandler={playlistHandler} currPlaylist={currPlaylist}/>
+        {page==="Profile" && <Profile
           className={styles.Profile}
           user={props.user}
           setUserHandler={props.setUserHandler}
         />
+        }
+        {page==="NewPlaylist" && <div>
+          <p>Create a new playlist</p>
+        </div>
+        }
+        {page==="Playlist" && <div>
+          <p>Playlist stranka</p>
+        </div>
+        }
+        
       </div>
 
       <Player />
