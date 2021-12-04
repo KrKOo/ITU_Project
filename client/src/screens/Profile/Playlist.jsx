38ba23e9b431df03536e5b58cb7853c9aea7e04c
OSBringer <bhlavienka@gmail.com>
2021-12-04 15:40:44 +0100

    Added tracks for testing purposes

--- client/src/screens/Profile/Playlist.jsx
+++ client/src/screens/Profile/Playlist.jsx
@@ -3,25 +3,29 @@ import { Link } from 'react-router-dom';
 import styles from './Playlist.module.scss';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import PauseCircleIcon from '@mui/icons-material/PauseCircle';
-
+import tracks from "../../tracks";
 const Playlist = (props) => {
-    const handleClick = (foo) => {
+  const [trackIndex, setTrackIndex] = useState(0);
+    const handleClick = (foo,index) => {
         if (props.currSong === foo) props.playHandler(!props.playing); 
         else {
             props.songHandler(foo)
             props.playHandler(true);
-            let index = songs.indexOf(foo);
+            let index =  Object.values(tracks).indexOf(index);
+            console.log("index"+index);
             let queue = [];
-            for (index; index < songs.length; index++) queue.push(songs[index]);
+            for (index; index < tracks.length; index++) queue.push(tracks[index]);
             props.queueHandler(queue);
         }
         
     }
     const songs = ["Suck", "On", "Deez", "Nuts"]
-    const listItems = songs.map((title, index) => (
-        <li key={index} onClick={e => { handleClick(title) }} >     {title} </li>
-      ));
-  
+
+    const { title, artist, audioSrc } = tracks[trackIndex];
+   
+    const listItems = Object.values(tracks).map((item,index) => (
+        <li  onClick={e => { handleClick(item,index) }} >     {item.title}  {item.artist}  </li>
+    ));
   return (
     <div className={`${styles.Playlist} ${props.className}`}>
       <header>
