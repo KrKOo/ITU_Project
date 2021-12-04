ce6053432b51672a950273ed83a263d83abd87bd
OSBringer <bhlavienka@gmail.com>
2021-12-04 15:40:44 +0100

    Added tracks for testing purposes

---
+++ client/src/tracks.js
@@ -0,0 +1,19 @@
+
+import bysbys from "./assets/bysbys.mp3";
+import xdd from "./assets/xdd.mp3";
+
+export default [
+  {
+    title: "Bysbys",
+    artist: "OSBringer",
+    audioSrc: bysbys,
+   
+  },
+  {
+    title: "xdd",
+    artist: "LubošBoomin",
+    audioSrc: xdd,
+    
+  },
+
+];
