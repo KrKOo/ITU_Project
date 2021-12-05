31abe974ec3cd738254aba094e8671b11e97073f
KrKOo <krkoo.123@gmail.com>
2021-12-05 04:03:57 +0100

    Page design

--- client/src/components/SidePanel.js
+++ client/src/components/SidePanel.js
@@ -2,23 +2,36 @@
 import styles from './SidePanel.module.scss';
 
 export const SidePanel = (props) => {
-  
+
 
   //Placeholder list, nahradi ho list s playlistami
   const numbers = ["Metal", "Rap", "Pop", "Cock", "Sock"];
   const listItems = numbers.map((number, index) => (
-    <button className={props.currPlaylist===number ? styles.activeButton : ""} onClick={e => {props.pageHandler("Playlist"); props.playlistHandler(number)}}><li key={index}> {number} </li></button>
+    <button className={props.currPlaylist === number ? styles.activeButton : ""} onClick={e => { props.pageHandler("Playlist"); props.playlistHandler(number) }}><li key={index}> {number} </li></button>
   ));
 
 
   return (
     <div className={`${styles.SidePanel} ${props.className}`}>
-      <h1>Edvánsd Mjúzik Pleja</h1>
-      <div >
-        <button className={props.page==="Profile" ? styles.activeButton : ""} onClick={e => {props.pageHandler("Profile"); props.playlistHandler("")}}>My profile</button>
-        <button className={props.page==="Search" ? styles.activeButton : ""} onClick={e => {props.pageHandler("Search"); props.playlistHandler("")}}>Search</button>
-        <button className={props.page==="NewPlaylist" ? styles.activeButton : ""} onClick={e => {props.pageHandler("NewPlaylist"); props.playlistHandler("")}}>New playlist</button>
-      </div>
+      <h2>Music Player</h2>
+      <ul>
+        <button
+          className={props.page === "Profile" && styles.activeButton}
+          onClick={e => { props.pageHandler("Profile"); props.playlistHandler("") }}>
+          My profile
+        </button>
+
+        <button
+          className={props.page === "Search" && styles.activeButton}
+          onClick={e => { props.pageHandler("Search"); props.playlistHandler("") }}>
+          Search
+        </button>
+        <button
+          className={props.page === "NewPlaylist" && styles.activeButton}
+          onClick={e => { props.pageHandler("NewPlaylist"); props.playlistHandler("") }}>
+          New playlist
+        </button>
+      </ul>
       <div>
         <h2>My playlists</h2>
         <ul>{listItems}</ul>
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,16 +1,15 @@
 import styles from './Home.module.scss';
-import { Link ,Navigate} from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import Profile from '../Profile/Profile';
-import Playlist from '../Profile/Playlist';
-import Login from '../Login/Login';
-import Search from '../Profile/Search';
+import Playlist from '../Playlist/Playlist';
+import Search from '../Search/Search';
 import { SidePanel } from '../../components/SidePanel';
 import { Player } from '../../components/Player';
 import { useState, useEffect } from 'react';
 import { Createplaylist } from '../Profile/Createplaylist';
 
 const Home = (props) => {
-  const [page, setPage] = useState("Home");
+  const [page, setPage] = useState('Profile');
   const [currPlaylist, setCurrPlaylist] = useState([]);
   const [currSong, setCurrSong] = useState([]);
   const [playing, setPlaying] = useState(false);
@@ -20,60 +19,73 @@ const Home = (props) => {
 
   useEffect(() => {
     if (playing) {
-      console.log("Playing: " + queue[0].title + " queue: ");
-      for (let i = 1; i < queue.length; i++) console.log(queue[i].title + ",");
-    }
-    else console.log("Pausing");
+      console.log('Playing: ' + queue[0].title + ' queue: ');
+      for (let i = 1; i < queue.length; i++) console.log(queue[i].title + ',');
+    } else console.log('Pausing');
   }, [playing, queue]);
 
-  const queueHandler = (foo) => { 
+  const queueHandler = (foo) => {
     setQueue(foo);
     setqueueIndex(0);
-  }
-  const indexHandler = (foo) => { 
+  };
+  const indexHandler = (foo) => {
     setqueueIndex(foo);
     setCurrSong(queue[foo]);
-  } 
+  };
   const songHandler = (foo) => {
-    setCurrSong(foo)
-  }
-  const logHandler = (foo) => {
-    setLogged(foo)
-  }
+    setCurrSong(foo);
+  };
   const pageHandler = (foo) => {
-    setPage(foo)
-  }
+    setPage(foo);
+  };
   const playlistHandler = (foo) => {
-    setCurrPlaylist(foo)
-  }
+    setCurrPlaylist(foo);
+  };
   const playHandler = (foo) => {
-    setPlaying(foo)
-  }
-  if(logged) return (
+    setPlaying(foo);
+  };
+  return (
     <div className={styles.Home}>
       <div className={styles.flexRow}>
-      {props.user.username==="" && <Navigate to={'/login' } />}
-        <SidePanel className={styles.SidePanel} page={page} pageHandler={pageHandler} playlistHandler={playlistHandler} currPlaylist={currPlaylist}/>
-        {page==="Profile" && <Profile
-          className={styles.Profile}
-          user={props.user}
-          setUserHandler={props.setUserHandler}
+        {props.user.username === '' && <Navigate to={'/login'} />}
+        <SidePanel
+          className={styles.SidePanel}
+          page={page}
+          pageHandler={pageHandler}
+          playlistHandler={playlistHandler}
+          currPlaylist={currPlaylist}
         />
-        }
-        {page==="NewPlaylist" && <Createplaylist/>}
-        {page==="Playlist" && <Playlist queueHandler={queueHandler} currPlaylist={currPlaylist} playing={playing} currSong={currSong} playHandler={playHandler} songHandler={songHandler}/>}
-        {page==="Search" && <Search/>}
-        
+        {page === 'Profile' && (
+          <Profile
+            className={styles.Profile}
+            user={props.user}
+            setUserHandler={props.setUserHandler}
+          />
+        )}
+        {page === 'NewPlaylist' && <Createplaylist />}
+        {page === 'Playlist' && (
+          <Playlist
+            queueHandler={queueHandler}
+            currPlaylist={currPlaylist}
+            playing={playing}
+            currSong={currSong}
+            playHandler={playHandler}
+            songHandler={songHandler}
+          />
+        )}
+        {page === 'Search' && <Search />}
       </div>
 
-      <Player playing={playing} queueIndex={queueIndex} indexHandler={indexHandler} audioSrc={!playing ? "":currSong.audioSrc} queue={queue} playHandler={playHandler} currSong={currSong}/>
-    </div>
-  );
-  //!!!!!!!!!!!!!!----------------logHandler treba potom v logine pouzit na nastavenie login state------------!!!!!!!!!!!!!
-  //este som to nespravil
-  else return(
-    <div>
-      <Login logHandler={logHandler}/>
+      <Player
+        className={styles.Player}
+        playing={playing}
+        queueIndex={queueIndex}
+        indexHandler={indexHandler}
+        audioSrc={!playing ? '' : currSong.audioSrc}
+        queue={queue}
+        playHandler={playHandler}
+        currSong={currSong}
+      />
     </div>
   );
 };
